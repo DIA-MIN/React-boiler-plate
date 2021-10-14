0958a13eba13d46ae210a9bb6b5dd570d05ade89
buildingsoup <mns0327@naver.com>
2021-10-14 15:55:30 +0900

    feat: add create secret key

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const {User} = require('./models/User');
 
-const pass = encodeURIComponent('#mns@0327#');
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// application/json
+app.use(express.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    `mongodb+srv://diamin:${pass}@myboilerplate.ltxyo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-  `
-  )
+  .connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World! Review Start!'));
+app.get('/', (req, res) => res.send('리액트 조아아~!'));
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 디비에 넣어준다
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({success: false, err});
+    }
+    return res.status(200).json({success: true});
+  });
+});
 
 app.listen(port, () => console.log(`Example ${port}`));
